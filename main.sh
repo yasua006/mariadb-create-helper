6fd43575580a6b6e74395503232dfbca931e87de
yasua006 <yasua006@osloskolen.no>
2026-05-13 15:02:35 +0200

    fix: ikke quote variabler igjen
    Dobbel quote ikke variables igjen - unngår "future" feil

--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ show_all_databases() {
 
 show_specific_db_tables() {
     # 1 - database name to use
-    sudo mariadb -D "${1}" -e "SHOW TABLES"
+    sudo mariadb -D ${1} -e "SHOW TABLES"
 }
 
 
@@ -19,9 +19,11 @@ show_all_from_table() {
     # 1 - database name to use
     # 2 - table name to show
 
-    sudo mariadb -D "${1}" -e "SELECT 1 AS non_empty FROM ${2} LIMIT 1"
-
-    sudo mariadb -D "${1}" -e "SELECT * FROM ${2}"
+    $(sudo mariadb -D ${1} -e "SELECT 1 AS non_empty FROM ${2} LIMIT 1")
+    
+    echo "Columns: $(sudo mariadb -D ${1} -e DESC ${2})"
+    echo "Rows:"
+    sudo mariadb -D ${1} -e "SELECT * FROM ${2}"
 }
 
 
